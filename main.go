3ceb403d41e2a871455a4fc8e07eaac84edd49dd
AnjasFedo <fedoaf98@gmail.com>
2024-02-02 10:01:02 +0700

    Imported Go Fiber & Main Functionality

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	
+	"github.com/gofiber/fiber"
 )
 
-func main() {
+func setRoutes(app *fiber.App) {
+	app.Get(GetLeads)
+
+	app.Get(GetLeadById)
+
+	app.Post(CreateLeadById)
+
+	app.post(DeleteLeadById)
+}
+
+func initDatabase() {
 	
+}
+
+func main() {
+	app := fiber.New()
+
+	setRoutes(app)
+
+	app.Listen(3000)
 }
