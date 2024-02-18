c09f05ba872188df1de825f28cfff225a5f68209
Anjasfedo <fedoaf98@gmail.com>
2024-02-18 23:26:51 +0700

    Build & Init Database

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Anjasfedo/go-fiber-crm/lead"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func setRoutes(app *fiber.App) {
@@ -22,14 +23,14 @@ func setRoutes(app *fiber.App) {
 func initDatabase() {
 	var err error
 
-	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	database.DBConn, err = gorm.Open("sqlite3", "/database/leads.db")
 	if err != nil {
-		panic("Failed connect to database")
+		panic(err)
 	}
 
 	fmt.Println("Connected to database")
 
-	database.DBConn.AutoMigrate(&lead.lead{})
+	database.DBConn.AutoMigrate(&lead.Lead{})
 
 	fmt.Println("Database migrated")
 }
